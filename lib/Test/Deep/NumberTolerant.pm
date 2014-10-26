e29def2716d29fb1ef8c780f4b183a65df7ded04
Karen Etheridge <ether@cpan.org>
2014-10-26 16:45:52 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.077, github profile.

---
+++ lib/Test/Deep/NumberTolerant.pm
@@ -0,0 +1,47 @@
+use strict;
+use warnings;
+package Test::Deep::NumberTolerant;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Test::Deep::NumberTolerant;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Test-Deep-NumberTolerant>
+(or L<bug-Test-Deep-NumberTolerant@rt.cpan.org|mailto:bug-Test-Deep-NumberTolerant@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Test::Deep::NumberTolerant;
+
+fail('this test is TODO!');
+done_testing;
